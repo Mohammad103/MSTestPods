a8d8621c50392c008616f6b7210eb210f6e27112
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-03-06 13:17:07 +0200

    Add test file

---
+++ Example/MSTestPods/Classes/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  MSTestPods
+//
+//  Created by Mohammad Shaker on 3/6/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+}
